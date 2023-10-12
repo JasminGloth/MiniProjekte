c7ac0ce70cc7ee3c3c98911d986bebaab9d29f3f
JasminGloth <jasmin.gloth@dci.education>
2023-10-12 13:34:01 +0200

    delete sidebar

--- src/pages/PageCheckboxFilter.tsx
+++ src/pages/PageCheckboxFilter.tsx
@@ -27,7 +27,7 @@ export const PageCheckboxFilter = () => {
 	};
 
 	const saisonList = ['Frühling', 'Sommer', 'Herbst', 'Winter'];
-	const kategoryList = ['Frühstück', 'Mittag', 'Gebäck'];
+	const kategoryList = ["Frühstück",'Mittag',"Gebäck"];
 
 	const handleSortOptionChange = (event: ChangeEvent<HTMLButtonElement>) => {
 		setSortOption(event.target.value);
@@ -51,83 +51,74 @@ export const PageCheckboxFilter = () => {
 
 	return (
 		<div className="page pageCheckboxFilter">
-			<div className='sidebar'>
+			<div>
+				<label htmlFor="sortOption">Sortieren nach:</label>
 				<div>
-					<label htmlFor="sortOption">Sortieren nach:</label>
-					<div className='firstChoice'>
-						<button
-							id="btn"
-							type="button"
-							value="saison"
-							onClick={(e) => handleSortOptionChange(e)}
-							className={sortOption === 'saison' ? 'active' : ''}
-						>
-							Jahreszeit
-						</button>
-						<button
-							id="btn"
-							type="button"
-							value="kategory"
-							onClick={(e) => handleSortOptionChange(e)}
-							className={
-								sortOption === 'kategory' ? 'active' : ''
-							}
-						>
-							Mahlzeit
-						</button>
-					</div>
+					<button
+						id="btn"
+						type="button"
+						value="saison"
+						onClick={(e) => handleSortOptionChange(e)}
+						className={sortOption === 'saison' ? 'active' : ''}
+					>
+						Jahreszeit
+					</button>
+					<button
+						id="btn"
+						type="button"
+						value="kategory"
+						onClick={(e) => handleSortOptionChange(e)}
+						className={sortOption === 'kategory' ? 'active' : ''}
+					>
+						Mahlzeit
+					</button>
 				</div>
-
-				<div className='secondChoice'>
-				{sortOption === 'saison' && (
-					<div>
-						{saisonList.map((saison) => (
-							<button
-								id="btn"
-								key={saison}
-								onClick={() => handleSaisonChange(saison)}
-							>
-								{saison}
-							</button>
-						))}
-					</div>
-				)}
-
-				{sortOption === 'kategory' && (
-					<div>
-						{kategoryList.map((kategory) => (
-							<button
-								id="btn"
-								key={kategory}
-								onClick={() => handleKategoryChange(kategory)}
-							>
-								{kategory}
-							</button>
-						))}
-					</div>
-				)}
-				</div>
-
-				<div className='thirdChoice'>
-				{filterValue !== '' && (
-					<div>
-						{[...new Set(meals.flatMap((meal) => meal.tags))].map(
-							(tag) => (
-								<button
-									id="btn"
-									key={tag}
-									onClick={() => handleTagChange(tag)}
-									className={
-										selectedTag === tag ? 'active' : ''
-									}
-								>
-									{tag}
-								</button>
-							)
-						)}
-					</div>
-				)}</div>
 			</div>
+
+			{sortOption === 'saison' && (
+				<div>
+					{saisonList.map((saison) => (
+						<button
+							id='btn'
+							key={saison}
+							onClick={() => handleSaisonChange(saison)}
+						>
+							{saison}
+						</button>
+					))}
+				</div>
+			)}
+
+			{sortOption === 'kategory' && (
+				<div>
+					{kategoryList.map((kategory) => (
+						<button
+							id='btn'
+							key={kategory}
+							onClick={() => handleKategoryChange(kategory)}
+						>
+							{kategory}
+						</button>
+					))}
+				</div>
+			)}
+
+			{filterValue !== '' && (
+				<div>
+					{[...new Set(meals.flatMap((meal) => meal.tags))].map(
+						(tag) => (
+							<button
+								id='btn'
+								key={tag}
+								onClick={() => handleTagChange(tag)}
+								className={selectedTag === tag ? 'active' : ''}
+							>
+								{tag}
+							</button>
+						)
+					)}
+				</div>
+			)}
 
 			<div className="meal">
 				{filteredMeals().map((meal) => (
