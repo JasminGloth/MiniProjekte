b14277343bf4e850c641c634e1488622cdc3c941
JasminGloth <jasmin.gloth@dci.education>
2023-10-19 09:37:07 +0200

    kalender auf deutsch umgestellt

--- src/pages/PageKalender.tsx
+++ src/pages/PageKalender.tsx
@@ -1,7 +1,20 @@
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import deLocale from '@fullcalendar/core/locales/de';
+import meals from '../data/meals.json';
+
 export const PageKalender = () => {
+
+	};
 	return (
 		<div className="page pageKalender">
-			<p>This is the Kalender page.</p>
+			<h1>Mein Kalender</h1>
+			<FullCalendar
+				plugins={[dayGridPlugin]}
+				initialView="dayGridMonth"
+				firstDay={1}
+				locale={deLocale}
+			/>
 		</div>
 	);
 };
